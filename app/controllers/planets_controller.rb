f9674917317fd2533d21e28fdee2b84156c0931e
Joel <j.n.brewster@gmail.com>
2016-05-24 16:45:54 +1000

    Current

---
+++ app/controllers/planets_controller.rb
@@ -0,0 +1,18 @@
+class PlanetsController < ApplicationController
+  def index
+  end
+  def new
+    @planet = Planet.new
+  end
+  def create
+    @planet = Planet.create(planet_params)
+    if @planet.valid?
+      redirect_to planets_path :flash => { :errors => @planet.errors.full_messages.join('. ')}
+    end
+  end
+  def show
+  end
+  def planet_params
+    params.required(:planet).permit(:name, :diameter, :rings, :moons, :order, :explored, :image, :distance_from_sun, :atmosphere, :solid)
+  end
+end
